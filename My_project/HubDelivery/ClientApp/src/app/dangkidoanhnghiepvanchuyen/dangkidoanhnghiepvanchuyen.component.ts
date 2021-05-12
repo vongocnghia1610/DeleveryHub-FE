d455270d0af68018b2669c8fd42557a6e3bdde84
ngoclam9200 <builam022000@gmail.com>
2021-05-13 00:12:53 +0700

    admin tao goi cho doanh nghiep , tao chuc nang thay doi mat khau , bo sung fetch api cho chuc nang dang ki doanh nghiep van chuyen

--- My_project/HubDelivery/ClientApp/src/app/dangkidoanhnghiepvanchuyen/dangkidoanhnghiepvanchuyen.component.ts
+++ My_project/HubDelivery/ClientApp/src/app/dangkidoanhnghiepvanchuyen/dangkidoanhnghiepvanchuyen.component.ts
@@ -1,15 +1,137 @@
 
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject, ApplicationModule} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {FormControl, FormsModule, Validators} from '@angular/forms';
+import { from } from 'rxjs/observable/from';
+import{FormGroup} from'@angular/forms';
+import {Observable} from 'rxjs';
+import { AuthServiceService } from '../auth-service.service';
+import { RouterModule } from '@angular/router';
+import {Router} from '@angular/router';
+import { ElementRef, ViewChild} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
 @Component({
   selector: 'app-dangkidoanhnghiepvanchuyen',
   templateUrl: './dangkidoanhnghiepvanchuyen.component.html',
   styleUrls: ['./dangkidoanhnghiepvanchuyen.component.css']
 })
 export class DangkidoanhnghiepvanchuyenComponent implements OnInit {
+  
+  formGroup : FormGroup;
+  loading: boolean = false;
+  @ViewChild('fileInput') fileInput: ElementRef;
+  @ViewChild('fileInput') fileInputdoc: ElementRef;
 
-  constructor() { }
+  doc: File;
+  logo: File;
 
-  ngOnInit(): void {
+  constructor(private http:HttpClient,
+    private router:Router,private fb: FormBuilder) { }
+  ngOnInit() {
+    // this.createForm();
+    this.initForm()
+    this.createForm()
   }
+ 
+    
+  createForm() {
+    this.formGroup = this.fb.group({
+      logo: null,
+      doc: null,
+      Email: ['', Validators.required],
+      TenDoanhNghiep: ['', Validators.required],
+     
+      SoDienThoai: ['', Validators.required],
+      DiaChi:['', Validators.required],
+      Password:['', Validators.required],
+     
+      CheckBox: ['', Validators.required],
+    });
+  }
+    
+  initForm(){
+    this.formGroup= new FormGroup({
+      TenDoanhNghiep: new FormControl("",[ Validators.required]),
+      logo: new FormControl("",[ Validators.required]), 
+      doc: new FormControl("",[ Validators.required]), 
+      SoDienThoai: new FormControl("",[ Validators.required]),  
+      Email: new FormControl("",[ Validators.required]),
+      Password: new FormControl("",[ Validators.required]),
+      DiaChi: new FormControl("",[ Validators.required]),
+      CheckBox: new FormControl("",[ Validators.required]),
+   
+    }); 
+
+  }
+  onFileChangedoc(e: File[]) {
+    this.doc = <File>e[0];
+    console.log(this.doc);
+  }
+  onFileChangelogo(e: File[]) {
+    this.logo = <File>e[0];
+    console.log(this.logo);
+  }
+  private prepareSave(): any {
+    let input = new FormData();
+    // This can be done a lot prettier; for example automatically assigning values by looping through `this.form.controls`, but we'll keep it as simple as possible here
+    input.append('doc', this.doc, this.doc.name);
+    input.append('logo', this.logo, this.logo.name);
+    input.append('Email', this.formGroup.get('Email').value);
+    input.append('TenDoanhNghiep', this.formGroup.get('TenDoanhNghiep').value);
+    input.append('SoDienThoai', this.formGroup.get('SoDienThoai').value);
+    input.append('DiaChi', this.formGroup.get('DiaChi').value);
+    input.append('Password', this.formGroup.get('Password').value);
+
+    return input;
+  }
+    dangkidoanhnghiepProces(){
+      if (this.formGroup.valid){
+        // const formModel = this.prepareSave();
+        // console.log(this.formGroup.value);
+     
+        this.loading = true;
+
+        this.http.post(`http:///54.255.93.14/auth/register-doanhnghiep`, this.prepareSave()) .subscribe(result=>
+        {
+          console.log(this.prepareSave());
+          console.log(result);
+          alert("Đăng kí thành công");
+        },error=>{
+          alert("Email này đã được sử dung");
+        }
+        );
+
+        setTimeout(() => {
+          this.loading = false;
+        }, 1000);
+      
+        // console.log(this.formGroup);
+        // console.log("I am server");
+        // let headers= new HttpHeaders();
+        
+      
+        // headers = headers.set('Content-Type', 'multipart/form-data').set('Accept','application/json' );
+      // this.http.post(`http://54.255.93.14/auth/register-doanhnghiep`,this.formGroup.value, {headers:headers}).subscribe((result) =>{
+      
+      //   if(result)
+      //    console.log( result);
+      //    alert("thành công");
+       
+          
+          
+        
+
+      // });
+
+     
+      }
+    
+  
+    else alert("Bạn chưa nhập đầy đủ thông tin");
+      
+  }
+  
+     
+    
+    
 }
---
+++ My_project/HubDelivery/ClientApp/src/app/taogoidoanhnghiepadmin/taogoidoanhnghiepadmin.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit, Inject, ApplicationModule} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {FormControl, FormsModule, Validators} from '@angular/forms';
+import { from } from 'rxjs/observable/from';
+import{FormGroup} from'@angular/forms';
+import {Observable} from 'rxjs';
+import { AuthServiceService } from '../auth-service.service';
+import { RouterModule } from '@angular/router';
+import {Router} from '@angular/router';
+@Component({
+  selector: 'app-taogoidoanhnghiepadmin',
+  templateUrl: './taogoidoanhnghiepadmin.component.html',
+  styleUrls: ['./taogoidoanhnghiepadmin.component.css']
+})
+export class TaogoidoanhnghiepadminComponent implements OnInit {
+
+  formGroup : FormGroup;
+  constructor(private http:HttpClient,
+    private router:Router) { }
+  ngOnInit() {
+    this.initForm();
+
+    
+  }
+  register(data):Observable<any>{
+  
+let headers= new HttpHeaders();
+var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+var token = currentUser.token; // your token
+console.log(token);
+headers = headers.set('Access-Control-Allow-Origin', '*').set('Authorization', token);
+
+    console.log("I am server");
+    return this.http.post(`http://54.255.93.14/admin/create-goidoanhnghiep`, data, {headers:headers});
+  }
+  initForm(){
+    this.formGroup= new FormGroup({
+      TenGoi: new FormControl("",[ Validators.required]),
+      ThongTin: new FormControl("", [Validators.required]),
+      ChiPhi: new FormControl("",[ Validators.required]),
+      HanSuDung: new FormControl("",[ Validators.required]),
+    SoDonHang: new FormControl("",[ Validators.required]),
+
+   
+    }); 
+
+  }
+ 
+    registergoi(){
+
+      if (this.formGroup.valid){
+      this.register(this.formGroup.value).subscribe((result) =>{
+       
+        
+        if(result)
+         console.log( result);
+        
+         alert("Đăng kí thành công");
+  
+          
+          
+        
+
+      });
+
+     
+     
+    }
+  
+    else alert("Bạn chưa nhập đầy đủ thông tin");
+  
+  }
+  
+     
+    
+    
+
+}
